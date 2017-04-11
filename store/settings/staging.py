de61025c8bc07725aac5fff2704d4798111405a0
Martin Stastny <me@martinstastny.cz>
2017-04-11 19:28:09 +0200

    heroku build

---
+++ store/settings/staging.py
@@ -0,0 +1,22 @@
+from .base import *
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+DEBUG = False
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- store/wsgi.py
+++ store/wsgi.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
