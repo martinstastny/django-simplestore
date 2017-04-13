731f30526a195c957d73175adf16e0af28be3cc0
Martin Stastny <me@martinstastny.cz>
2017-04-13 15:59:03 +0200

    maange

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 
 sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 
     from django.core.management import execute_from_command_line
 
