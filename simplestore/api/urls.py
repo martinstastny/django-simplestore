e81f1bd09959ca43eec43456e1aa26dd913b9331
Martin Stastny <me@martinstastny.cz>
2017-05-15 01:26:33 +0200

    Cart Item update

--- simplestore/api/urls.py
+++ simplestore/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import ProductListView, CartView
+from . import views
 
 urlpatterns = [
-    url('^products/', ProductListView.as_view(), name='products'),
-    url('^cart/', CartView.as_view(), name='cart'),
+    url('^products/$', views.ProductListView.as_view(), name='products'),
+    url('^cart/$', views.CartDetailView.as_view(), name='cart'),
+    url(r'^cart/update/(?P<id>[0-9]+)/$', views.CartUpdateView.as_view(), name='update'),
 ]
--- simplestore/api/views.py
+++ simplestore/api/views.py
@@ -1,9 +1,12 @@
+from django.http import Http404
 from rest_framework.views import APIView
+from rest_framework import status
 from rest_framework.response import Response
 
 from simplestore.products.models.product import Product
+from simplestore.cart.models import CartItem
 from simplestore.cart.mixins import get_cart
-from .serializers import ProductSerializer, CartSerializer
+from .serializers import ProductSerializer, CartSerializer, CartItemSerializer
 
 
 class ProductListView(APIView):
@@ -17,7 +20,7 @@ class ProductListView(APIView):
         return Response(serializer.data)
 
 
-class CartView(APIView):
+class CartDetailView(APIView):
     '''
     Cart detail view
     '''
@@ -26,3 +29,24 @@ class CartView(APIView):
         cart = get_cart(request)
         serializer = CartSerializer(cart)
         return Response(serializer.data)
+
+
+class CartUpdateView(APIView):
+    '''
+    Cart update view
+    '''
+
+    def get_object(self, id):
+        try:
+            cart = get_cart(self.request)
+            return cart.items.get(pk=id)
+        except CartItem.DoesNotExist:
+            raise Http404
+
+    def patch(self, request, id):
+        item = self.get_object(id)
+        serializer = CartItemSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
