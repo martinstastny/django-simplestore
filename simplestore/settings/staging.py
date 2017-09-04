b86966b3466bae488b5690c09aea145df969b0f3
Martin Stastny <me@martinstastny.cz>
2017-09-04 22:52:41 +0200

    Update staging.py
    Disable debug on staging.

--- simplestore/settings/staging.py
+++ simplestore/settings/staging.py
@@ -5,7 +5,7 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-DEBUG = True
+DEBUG = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
